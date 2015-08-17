fac49d93dccd5010086dbf4f21319c2ceca7002e
Davide Zilli <zilli.davide@gmail.com>
2015-08-17 11:31:42 +0100

    remove reminescence of old project

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,4 @@
-"""wwwhb/project URL Configuration
+"""soundtrap/project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
--- soundtrap/wsgi.py
+++ soundtrap/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for wwwhb project.
+WSGI config for soundtrap project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wwwhb.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "soundtrap.settings")
 
 application = get_wsgi_application()
